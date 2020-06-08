e5cd02ed2ea7d5275856d1b7a1648b5304f772ee
Magno Stiven Bueno <magno12.mcmb@gmail.com>
2020-06-08 16:29:23 -0500

    add mas codigo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const http = require('./app');
+const server = http.createServer(app);
+const io = Socketio.listen(server);
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + "/public"));
 
 http.listen(3000, () => {
         console.log('servidor en puerto 3000 funcionando');
+});
+
+server.listen(app.get('port'), () => {
+        console.log(`server on port ${app.get('port')}`);
 });
